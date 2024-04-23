c8d3280c79161c4b68c1a10f2d26e4ee4c237781
shivank mishra <shivankmishra710@gmail.com>
2024-04-23 11:26:45 +0530

    correct callender

--- src/pages/RgpvCalender/RgpvCalender.jsx
+++ src/pages/RgpvCalender/RgpvCalender.jsx
@@ -11,7 +11,7 @@ const AcademicCalendar = () => {
         'Dussehra Holiday': '22nd to 25th October 2023',
         'Submission of Examination form': {
           'Without late fee': '25th Oct. - 13th Nov. 2023',
-          'With late fee': '13th - 19th November 2023',
+          'With late fee ': '13th - 19th November 2023',
         },
         'Submission of Mid Semester & Sessional Marks to University': '13th to 20th November 2023',
         'Last date of Teaching': '10th November 2023',
@@ -20,7 +20,7 @@ const AcademicCalendar = () => {
           'Theory': '20th Nov. - 22nd Dec. 2023',
           'Practical Examination': '20th Nov. - 22nd Dec. 2023',
         },
-        'Winter/Summer Vacation for Teachers': '23rd to 31st December 2023',
+        'Winter*/Summer Vacation for Teachers': '23rd to 31st December 2023',
         'Declaration of result of final Year': 'In the Month of January 2024',
       },
     },
@@ -32,7 +32,7 @@ const AcademicCalendar = () => {
         'Mid Semester Exam (II)': '18th to 23rd March 2024',
         'Submission of Examination form': {
           'Without late fee': '01st - 19th April 2024',
-          'With late fee': '20th - 26th April 2024',
+          'With late fee    ': '20th - 26th A ril 2024',
         },
         'Last date of Teaching': '19th April 2024',
         'Submission of Mid Semester & Sessional Marks to University': '20th - 27th April 2024',
@@ -41,11 +41,54 @@ const AcademicCalendar = () => {
           'Practical Examination': '30th April - 31st May 2024',
         },
         'Summer Internship (Optional)': '01st - 30th June 2024',
-        'Winter/Summer Vacation for Teachers': '01st - 30th June 2024',
-        'Declaration of result of final Year': 'In the Month of June 2024',
+        'Winter*/Summer Vacation for Teachers':'01st - 30th June 2024',
+        'Declaration of result of final Year':'In the Month of June 2024',
       },
     },
-    // Your additional semesters here
+    {
+      "semester": "III & V Semester",
+      "schedule": {
+        "Commencement of Session": "01 August 2023",
+        "Mid Semester Exam (I)": "15th-22nd Sep. 2023",
+        "Mid Semester Exam (II)": "26th Oct. - 01st Nov. 2023",
+        "Dussehra Holiday": "22nd-25th Oct. 2023",
+        "Submission of Examination form": {
+          "Without late fee": "01st-17th Nov. 2023",
+          "With late fee": "18th-27th Nov. 2023"
+        },
+        "Diwali Vacation": "11th-15th Nov. 2023",
+        "Last date of Teaching": "18th November 2023",
+        "End Semester Examination": {
+          "Theory": "28th Nov.- 27th Dec. 2023",
+          "Practical Examination": "On the date of Practical Examination"
+        },
+        "Submission of Mid Semester & Sessional Marks to University": "16th-26th Nov. 2023",
+        "End Semester Break/Internship": "NA",
+        "Winter Vacation for teachers": "23rd-31st December 2023",
+        "Declaration of Result": "In the Month of June 2024"
+      }
+    },
+    {
+      "semester": "IV & VI Semester",
+      "schedule": {
+        "Commencement of Session": "05th January 2024",
+        "Mid Semester Exam (I)": "22nd-28th February 2024",
+        "Mid Semester Exam (II)": "18th-23rd March 2024",
+        "Submission of Examination form": {
+          "Without late fee": "01st-19th April 2024",
+          "With late fee": "20th-24th April 2024"
+        },
+        "Last date of Teaching": "25th April 2024",
+        "Submission of Mid Semester & Sessional Marks to University": "15th-26th April 2024",
+        "End Semester Examination": {
+          "Theory": "25th April-31st May 2024",
+          "Practical Examination": "On the date of Practical Examination"
+        },
+        "End Semester Break/Internship": "01st-30th June 2024",
+        "Winter/summer Vacation for teachers": "30th June 2024",
+        "Declaration of Result": "In the Month of June 2024"
+      }
+    }
   ];
 
   const [selectedSemester, setSelectedSemester] = useState(semesters[0]);
